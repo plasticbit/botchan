9f441e61230e96547e0caed330c20bee8f541608
yuuyuu1396 <m4160717428@yahoo.co.jp>
2019-11-05 01:41:19 +0900

    position test

--- src/event/client/message/newQuestion.js
+++ src/event/client/message/newQuestion.js
@@ -13,13 +13,15 @@ module.exports = {
         const channels = guild.channels
         const filter = channels.filter(ch => ch.name === channel.name)
 
-
-        if (filter.size >= 3) {
+        if (channel.parentID !== "393442427912060928") {
+            message.reply("このコマンドは`LANG`カテゴリーのみ有効です。")
+        } else if (filter.size >= 3) {
             message.reply("チャンネルが複数あります。\nこれ以上チャンネルを作ることはできません！", global.syntax)
         } else {
             const newChannel = await guild.createChannel(channel.name, { type: "text" })
             await newChannel.setTopic(`${message.author.id},${Date.now()}`)
             await newChannel.setParent("393442427912060928")
+            await newChannel.setPosition(channel.position+1)
             await newChannel.send(`このチャンネルは ${message.member.toString()} によって作成されました！\n__\`closeコマンドによって、チャンネルが自動的に削除されます\`__`)
             
             // LANG-CATEGORY: 640931602309971970
