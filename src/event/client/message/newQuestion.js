5ec6cc16014a39af8e8e564c4de27ca06a5e2b0a
yuuyuu1396 <m4160717428@yahoo.co.jp>
2019-11-05 01:17:55 +0900

    commit

--- src/event/client/message/newQuestion.js
+++ src/event/client/message/newQuestion.js
@@ -13,13 +13,14 @@ module.exports = {
         const channels = guild.channels
         const filter = channels.filterArray(ch => ch.name === channel.name)
 
-        message.guild.c
 
         if (filter.length >= 3) {
             message.reply("チャンネルが複数あります。\nこれ以上チャンネルを作ることはできません！", global.syntax)
         } else {
             const newChannel = await guild.createChannel(channel.name, { type: "text" })
+            await newChannel.setTopic(`author: ${message.author.id}`)
             await newChannel.setParent("393442427912060928")
+            await newChannel.send(`このチャンネルは ${message.member.toString()} によって作成されました！\n_\`closeコマンドによって、チャンネルが自動的に削除されます\`_`)
             
             // LANG-CATEGORY: 640931602309971970
             // TEST-CATEGORY: 393442427912060928
