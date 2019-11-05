eec078d4fb12b3863228dcd70dd34ca2b1a18cb3
yuuyuu1396 <m4160717428@yahoo.co.jp>
2019-11-06 01:37:33 +0900

    commit

--- src/event/client/message/newQuestion.js
+++ src/event/client/message/newQuestion.js
@@ -12,25 +12,20 @@ module.exports = {
         const channel = message.channel
         const channels = guild.channels
         const filter = channels.filter(ch => ch.name === channel.name)
-        const howmany = filter.filter(ch => ch.topic)
-
-        console.log(howmany)
 
         if (channel.parentID !== "640931602309971970") {
             message.reply("このコマンドは`LANG`カテゴリーのみ有効です。")
-        } else if (filter.size >= 3 || filter.fo) {
+        } else if (filter.size >= 3) {
             message.reply("チャンネルが複数あります。\nこれ以上チャンネルを作ることはできません！", global.syntax)
         } else {
             // console.log(guild.channels.filter(c => c.parentID === "640931602309971970").map(c => `${c.name}: ${c.calculatedPosition}`))
 
-            // ひとり1チャンネルまで？？？
-
-
             const newChannel = await guild.createChannel(channel.name, { type: "text" })
             await newChannel.setTopic(`${message.author.id},${Date.now()}`)
             await newChannel.setParent("640931602309971970")
-            // await newChannel.setPosition(channel.position++) || "？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？"
+            // await newChannel.setPosition(channel.position++)
             await newChannel.send(`このチャンネルは ${message.member.toString()} によって作成されました！\n__\`解決すれば closeコマンドを実行し、チャンネルを削除してください\`__`)
+            channel.send(newChannel.toString())
             
             // LANG-CATEGORY: 640931602309971970
             // TEST-CATEGORY: 393442427912060928
