9cda87e5c981b3afa26f98ec8f035da263060186
yuuyuu1396 <m4160717428@yahoo.co.jp>
2019-12-02 02:16:37 +0900

    胃

--- src/event/client/message/react.js
+++ src/event/client/message/react.js
@@ -54,4 +54,11 @@ module.exports = {
 
     }
 }
-// aaa
+/*
+https://discord.js.org/#/docs/main/stable/class/Message?scrollTo=createReactionCollector
+
+const filter = (reaction, user) => reaction.emoji.name === '👌' && user.id === 'someID'
+const collector = message.createReactionCollector(filter, { time: 15000 });
+collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+*/
