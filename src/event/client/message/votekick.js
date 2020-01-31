1dda6616a2d57c3e4bfd29f283cb774583f04260
yuuyuu1396 <m4160717428@yahoo.co.jp>
2020-01-31 19:08:08 +0900

    edit votekick.js

--- src/event/client/message/votekick.js
+++ src/event/client/message/votekick.js
@@ -1,12 +1,36 @@
-const { Message } = require("discord.js")
+const { Message, MessageMentions: { USERS_PATTERN } } = require("discord.js")
 
 module.exports = {
-    usage: "",
-    examples: "",
-    description: "",
+    usage: "b;votekick @MENTION REASON",
+    examples: "b;votekick @MENTION REASON",
+    description: "迷惑なユーザーをサーバーからキックするコマンドです。",
 
     /** @param {Message} message **/
     Do: message => {
-        const memtion = message.mentions.members.first()
+        const mentions = message.mentions.members
+        const channel = message.channel
+        const reason = message.args[2]
+
+        if (mentions.size() !== 1 || !USERS_PATTERN.test(message.args[0])) {
+            channel.send("引数が無効です。\n\n例: b;votekick @MENTION REASON", global.syntax)
+        } else {
+            const member = mentions.first()
+            const voteMessage = channel.send({
+                embed: {
+                    color: 0xFF0000,
+                    author: {
+                        name: message.member.displayName,
+                        icon_url: message.author.displayAvatarURL
+                    },
+                    title: "このユーザーをキックしますか？",
+                    image: {
+                        url: member.displayAvatarURL
+                    }
+                }
+            })
+        }
     }
 }
+
+// b;votekick @MENTION REASON
+// index:        0       1
