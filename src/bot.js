21f47691555ae9f5f339905126fc873d8f9c4031
yuuyuu1396 <m4160717428@yahoo.co.jp>
2020-11-01 12:25:41 +0900

    Discord.jsのバージョンを変更

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,10 @@ const client = new Client({
     messageCacheMaxSize: 500,
     messageCacheLifetime: 120,
     messageSweepInterval: 60,
+    ws: {
+        intents: "GUILD_MEMBERS",
+        intents: "GUILD_PRESENCES"
+    }
 })
 
 // ログイン
